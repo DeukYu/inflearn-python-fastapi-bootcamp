c95c588fcc5fa8cf66aee83128ae56aeb48b788a
DeukYu <ldy8070@naver.com>
2024-04-08 22:13:34 +0900

    start

---
+++ 00_FASTAPI_SAMPLECODES/00_START/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/inherit")
+def template_inherit(request: Request):
+    my_text = "FastAPI와 Jinja2를 이용한 예시입니다."
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "text": my_text}
+    )
