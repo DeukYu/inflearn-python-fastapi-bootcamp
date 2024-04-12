f36b5295c1e5fc76597bc1a8660a8e1086b35792
DeukYu <ldy8070@naver.com>
2024-04-12 22:34:59 +0900

    Update main.py

--- 00_FASTAPI_SAMPLECODES/00_START/main.py
+++ 00_FASTAPI_SAMPLECODES/00_START/main.py
@@ -37,3 +37,13 @@ def template_inherit(request: Request):
     return templates.TemplateResponse(
         "index.html", {"request": request, "text": my_text}
     )
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: dict):
+    return {"item_id": item_id, "updated_item": item}
+
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": f"Item {item_id} has been deleted."}
