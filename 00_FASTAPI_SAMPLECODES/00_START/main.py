16629d140939d8dcbf810f139b7a4e8f50274769
DeukYu <ldy8070@naver.com>
2024-04-23 22:04:12 +0900

    Update main.py

--- 00_FASTAPI_SAMPLECODES/00_START/main.py
+++ 00_FASTAPI_SAMPLECODES/00_START/main.py
@@ -1,8 +1,34 @@
 import asyncio
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from contextlib import asynccontextmanager
+
+DATABASE_URL = "mysql+aiomysql://ldy8070:deukyu@localhost/db_name"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+ASYNC_SESSION = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
+)
+
+Base = declarative_base()
 
 
-app = FastAPI()
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=app_lifespan)
+
+
+async def get_db():
+    async with ASYNC_SESSION() as session:
+        yield session
+        await session.commit()
 
 
 async def fetch_data():
